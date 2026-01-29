beedf426a96393a5e14534063007f97e04fd17a8
Mishu78 <meherun2002@gmail.com>
2026-01-29 12:13:36 +0600

    useEffect used

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
+    <> 
     <h3>React core concepts</h3>
     <ol>
       <li>Components</li>
-      <li>Components</li>
+      <li>Jsx</li>
+      <li>Props</li>
+      <li>Event Handler</li>
+      <li>State</li>
+      <li>Load data</li>
     </ol>
     <Posts></Posts>
       
--- src/posts.jsx
+++ src/posts.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
 export default function Posts(){
+    const [posts,setPosts]=useState([])
+   useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then (res => res.json())
+    .then (data => setPosts(data))
+   },[])
     return(
         <div className="">
             <h3>Posts: </h3>
         </div>
     )
 }
+
+
+
+/*create a state to store the data
+  create use effect with no dependencies
+*/
+
